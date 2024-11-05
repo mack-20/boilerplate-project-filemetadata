2c546938d4b0c33fd727fdf4c9d88c5bb5f88ff8
mack-20 <mackleodebenboulo@gmail.com>
2024-11-05 11:18:28 +0000

    Did this without following any tutorial

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
+const path = require('path')
 require('dotenv').config()
 
-var app = express();
+const app = express();
+
+// Configure storage engine and filename
+const storage = multer.diskStorage({
+  destination: './uploads',
+  filename: function(req, file, cb){
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+
+// Initialize upload middleware and add file size limit
+const upload = multer({
+  storage: storage,
+}).single('upfile')
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +26,30 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Set POST route for file upload
+app.post('/api/fileanalyse', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      console.error(err)
+      return res.status(500).json({error: err})
+    }
+    if(!req.file){
+      return res.status(400).json({error: 'Please send file'})
+    }
+    
+    const uploadedFile = req.file
+    const filename = uploadedFile.originalname
+    const filesize_bytes = uploadedFile.size
+    const mimetype = uploadedFile.mimetype
+
+    console.log(req.file)
+    res.json({
+      name: filename,
+      type: mimetype,
+      size: filesize_bytes
+    })
+  })
+})
 
 
 
